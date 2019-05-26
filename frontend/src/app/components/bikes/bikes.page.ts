7bdf9b0342662759b2f1ee8564c63b0e821f076b
mbasart <mbd2797@gmail.com>
2019-05-26 16:27:38 +0200

    he afegit toast amb ionic

--- frontend/src/app/components/bikes/bikes.page.ts
+++ frontend/src/app/components/bikes/bikes.page.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import { Bike } from 'src/app/models/bike';
 import { Station } from 'src/app/models/station';
 import { ServiceService } from 'src/app/Services/Service.Service';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -14,7 +15,7 @@ import { ServiceService } from 'src/app/Services/Service.Service';
 })
 export class BikesPage implements OnInit {
 
-  constructor(private serviceService: ServiceService, private router: Router) { 
+  constructor(private serviceService: ServiceService, private router: Router,public toastController: ToastController) { 
     if(this.router.getCurrentNavigation().extras.queryParams!=undefined){
       this.serviceService.selectedStation = this.router.getCurrentNavigation().extras.queryParams.station;
     }
@@ -35,6 +36,14 @@ export class BikesPage implements OnInit {
     });
   }
 
+  async presentToastAdd() {
+    const toast = await this.toastController.create({
+      message: 'Added Successfully',
+      duration: 2000
+    });
+    toast.present();
+  }
+
   addBike(bike) {
     this.serviceService.selectedBike = bike;
     this.serviceService.addBike(this.serviceService.selectedStation._id, bike)
@@ -42,7 +51,16 @@ export class BikesPage implements OnInit {
         this.getStation();
         this.getUnassignedBikes();
         //M.toast({html: 'Added Successfully'});
+        this.presentToastAdd();
       });
+  }
+
+  async presentToastDelete() {
+    const toast = await this.toastController.create({
+      message: 'Deleted Successfully',
+      duration: 2000
+    });
+    toast.present();
   }
 
   deleteBike(bike) {
@@ -52,6 +70,7 @@ export class BikesPage implements OnInit {
         this.getStation();
         this.getUnassignedBikes();
        // M.toast({html: 'Deleted Successfully'});
+       this.presentToastDelete();
       });
   }
 
